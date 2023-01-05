4b5df5c20a88f3729f1e6168cbdf0c934d19c3b1
Shish-ok <alexeyshish92@gmail.com>
2023-01-05 17:36:20 +0300

    make jwt redis

--- internal/app/repository/redis_repository/jwt_sessions.go
+++ internal/app/repository/redis_repository/jwt_sessions.go
@@ -1,14 +1,18 @@
 package redis_repository
 
 import (
+	"strconv"
 	"time"
 )
 
 const (
-	restoreEmailRate = "restore_rate:"
-	restoreUID       = "restore_uid:"
+	RefreshTemplateKey = "refresh_"
 )
 
-func (r *RedisRepository) CreateJWTSession(token, refresh string, at time.Time) error {
+func (r *RedisRepository) CashRefreshToken(uid uint64, refresh string, at time.Duration) error {
+	return r.db.Set(RefreshTemplateKey+strconv.Itoa(int(uid)), refresh, at).Err()
+}
 
+func (r *RedisRepository) GetRefreshToken(uid uint64) (string, error) {
+	return r.db.Get(RefreshTemplateKey + strconv.Itoa(int(uid))).Result()
 }
