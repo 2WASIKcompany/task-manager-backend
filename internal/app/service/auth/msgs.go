4500d46e23ae590d33afc02a93d2212fb1175a1a
Shish-ok <alexeyshish92@gmail.com>
2023-01-17 23:45:24 +0300

    refactor docker

--- internal/app/service/auth/msgs.go
+++ internal/app/service/auth/msgs.go
@@ -1,6 +1,6 @@
 package auth
 
 const (
-	confimEmailMsg = "To: %s\r\nFrom: alexeyshish92@gmail.com\r\nSubject: Подверждение почты\r\n\r\nДля подверждения регистрации перейдите по ссылке: https://mytodo.ru/confirm/%s"
-	changePassMsg  = "To: %s\r\nFrom: alexeyshish92@gmail.com\r\nSubject: Смена пароля\r\n\r\nДля смены пароля перейдите по ссылке: https://mytodo.ru/restore_password/%s"
+	confimEmailMsg = "To: %s\r\nFrom: alexeyshish92@gmail.com\r\nSubject: Подверждение почты\r\n\r\nДля подверждения регистрации перейдите по ссылке: https://task-meneger-web-svelte.vercel.app/auth/confirm/%s"
+	changePassMsg  = "To: %s\r\nFrom: alexeyshish92@gmail.com\r\nSubject: Смена пароля\r\n\r\nДля смены пароля перейдите по ссылке: https://task-meneger-web-svelte.vercel.app/auth/restore_password/%s"
 )
